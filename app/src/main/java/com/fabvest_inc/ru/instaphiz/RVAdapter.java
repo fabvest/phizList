8f4b43dcd20b229eed414d5e835d7ac597f9ad68
fab <fabvest7@gmail.com>
2017-04-08 18:34:01 +0300

    adding Alert dialog for delete DB string

--- app/src/main/java/com/fabvest_inc/ru/instaphiz/RVAdapter.java
+++ app/src/main/java/com/fabvest_inc/ru/instaphiz/RVAdapter.java
@@ -1,6 +1,8 @@
 package com.fabvest_inc.ru.instaphiz;
 
-import android.support.annotation.Nullable;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -8,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.fabvest_inc.ru.instaphiz.Data.User;
 
@@ -16,6 +19,15 @@ import java.util.List;
 
 public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ViewHolder> {
     private List<User> rvUsers;
+    AlertDialog.Builder alertDialog;
+    private String title = "Подтвердите действие";
+    private String message = "Удалить запись о ";
+    private String button1 = "Да";
+    private String button2 = "Нет";
+    private String message2;
+    Context context;
+    boolean flag;
+
 
     public RVAdapter(List<User> user){
         this.rvUsers = user;
@@ -30,8 +42,8 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        context = holder.itemView.getContext();
         if(holder == null) Log.d("holder", "holder is null");
         holder.mNameText.setText(rvUsers.get(position).getmName());
         holder.mLessonText.setText(rvUsers.get(position).getLesson());
@@ -39,6 +51,37 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ViewHolder> {
         holder.mPriceText.setText(String.valueOf(rvUsers.get(position).getPrice()));
         holder.mSemesterText.setText(String.valueOf(rvUsers.get(position).getSemester()));
         holder.isKafedraText.setChecked(rvUsers.get(position).isKafedra());
+        alertDialog = new AlertDialog.Builder(context);
+        alertDialog.setTitle(title);
+        alertDialog.setMessage(message);
+        alertDialog.setPositiveButton(button1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                flag = true;
+            }
+        });
+        alertDialog.setNegativeButton(button2, new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                flag = false;
+            }
+        });
+        alertDialog.setCancelable(true);
+        alertDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            public void onCancel(DialogInterface dialog) {
+                flag = false;
+            }
+        });
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                alertDialog.show();
+                Context context = v.getContext();
+                //// TODO: 08.04.2017 написать обработчик нажатия на удаление элемента БД 
+            }
+        });
+
     }
 
     @Override
@@ -46,7 +89,9 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ViewHolder> {
         return rvUsers.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
         public TextView mNameText;
         public TextView mLessonText;
         public TextView mMarkText;
@@ -55,15 +100,16 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ViewHolder> {
         public CheckBox isKafedraText;
 
 
+
         public ViewHolder(View itemView) {
             super(itemView);
-            mNameText = (TextView)itemView.findViewById(R.id.textViewName);
+            mNameText = (TextView) itemView.findViewById(R.id.textViewName);
             mLessonText = (TextView) itemView.findViewById(R.id.textViewLesson);
             mMarkText = (TextView) itemView.findViewById(R.id.textViewMark);
             mPriceText = (TextView) itemView.findViewById(R.id.textViewPrice);
-            mSemesterText =(TextView) itemView.findViewById(R.id.textViewSemester);
+            mSemesterText = (TextView) itemView.findViewById(R.id.textViewSemester);
             isKafedraText = (CheckBox) itemView.findViewById(R.id.checkBox2);
-
         }
+
     }
 }
